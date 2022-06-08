2651acf173af982a753eec40c121e2c3a994c116
Oui-Dev <Oui-Dev@github.com>
2022-06-08 14:27:00 +0200

    refactor: Organisation des fichiers

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+import logo from '../assets/logo.svg'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p className="underline">
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/app/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
