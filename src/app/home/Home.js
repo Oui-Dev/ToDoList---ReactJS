ec7d103447f96d11e07572e0a4a4c7852468939f
Oui-Dev <Oui-Dev@github.com>
2022-06-09 15:14:39 +0200

    feat: drag&drop fonctionnel + localstorage

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -1,55 +1,92 @@
 import './Home.scss'
-import React, { useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Home() {
     const dragItem = useRef()
-    const dropInList = useRef()
-    const [todoList, setTodoList] = useState(['todo 1', 'todo 2', 'todo 3', 'todo 4', 'todo 5', 'todo 6'])
-    const [doingList, setDoingList] = useState(['doing 1', 'doing 2', 'doing 3', 'doing 4', 'doing 5', 'doing 6'])
-    const [doneList, setDoneList] = useState(['done 1', 'done 2', 'done 3', 'done 4', 'done 5', 'done 6'])
+    const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('todoList')))
+    const [doingList, setDoingList] = useState(JSON.parse(localStorage.getItem('doingList')))
+    const [doneList, setDoneList] = useState(JSON.parse(localStorage.getItem('doneList')))
 
-    const onDrag = (e, item) => {
-        console.log(e.target)
-        dragItem.current = item
+    function onDrag(list, i) {
+        dragItem.index = i
+
+        switch (list) {
+            case 1:
+                dragItem.array = [...todoList]
+                todoList.splice(i, 1)
+                setTodoList(todoList)
+                break
+            case 2:
+                dragItem.array = [...doingList]
+                doingList.splice(i, 1)
+                setDoingList(doingList)
+                break
+            case 3:
+                dragItem.array = [...doneList]
+                doneList.splice(i, 1)
+                setDoneList(doneList)
+                break
+            default:
+                console.log('Error')
+        }
     }
-    const onDragOver = (e) => {
+    function onDragOver(e) {
         e.preventDefault()
     }
-    const onDrop = (e, item) => {
-        console.log(e.target)
-        dropInList.current = item
+    function onDrop(list) {
+        const item = dragItem.array[dragItem.index]
+
+        switch (list) {
+            case 1:
+                localStorage.setItem('todoList', JSON.stringify([...todoList, item]))
+                setTodoList([...todoList, item])
+                break
+            case 2:
+                localStorage.setItem('doingList', JSON.stringify([...doingList, item]))
+                setDoingList([...doingList, item])
+                break
+            case 3:
+                localStorage.setItem('doneList', JSON.stringify([...doneList, item]))
+                setDoneList([...doneList, item])
+                break
+            default:
+                console.log('Error')
+        }
     }
 
     return (
         <div className="homeContent">
-            <div id="todo" className="itemBox" onDrop={(e) => onDrop(e, 1)} onDragOver={(e) => onDragOver(e)}>
+            <div id="todo" className="itemBox">
                 <h3>ToDo</h3>
-                <div>
-                    {todoList.map((item, index) => (
-                        <div className="item" onDragStart={(e) => onDrag(e, index)} key={index} draggable>
-                            {item}
-                        </div>
-                    ))}
+                <div className="h-full" onDrop={() => onDrop(1)} onDragOver={(e) => onDragOver(e)}>
+                    {todoList &&
+                        todoList.map((item, index) => (
+                            <div className="item" onDragStart={() => onDrag(1, index)} key={index} draggable>
+                                {item}
+                            </div>
+                        ))}
                 </div>
             </div>
-            <div id="doing" className="itemBox" onDrop={(e) => onDrop(e, 2)} onDragOver={(e) => onDragOver(e)}>
+            <div id="doing" className="itemBox">
                 <h3>Doing</h3>
-                <div>
-                    {doingList.map((item, index) => (
-                        <div className="item" onDragStart={(e) => onDrag(e, index)} key={index} draggable>
-                            {item}
-                        </div>
-                    ))}
+                <div className="h-full" onDrop={() => onDrop(2)} onDragOver={(e) => onDragOver(e)}>
+                    {doingList &&
+                        doingList.map((item, index) => (
+                            <div className="item" onDragStart={() => onDrag(2, index)} key={index} draggable>
+                                {item}
+                            </div>
+                        ))}
                 </div>
             </div>
-            <div id="done" className="itemBox" onDrop={(e) => onDrop(e, 3)} onDragOver={(e) => onDragOver(e)}>
+            <div id="done" className="itemBox">
                 <h3>Done</h3>
-                <div>
-                    {doneList.map((item, index) => (
-                        <div className="item" onDragStart={(e) => onDrag(e, index)} key={index} draggable>
-                            {item}
-                        </div>
-                    ))}
+                <div className="h-full" onDrop={() => onDrop(3)} onDragOver={(e) => onDragOver(e)}>
+                    {doneList &&
+                        doneList.map((item, index) => (
+                            <div className="item" onDragStart={() => onDrag(3, index)} key={index} draggable>
+                                {item}
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
